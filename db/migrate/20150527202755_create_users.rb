4ed0a8b6c59e62498a8113322a8e738402681152
Michał Zagól <mysza1179@gmail.com>
2015-05-27 20:58:42 +0000

    db migration

--- db/migrate/20150527202755_create_users.rb
+++ db/migrate/20150527202755_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :password, null: false
+      t.string :name, null: true
+      t.string :lastname, null: true
       t.timestamps null: false
     end
   end
